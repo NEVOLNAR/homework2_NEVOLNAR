bd19fc7cc1d058bd68773696ed42d3169ea3a5f2
NEVOLNAR <artemnevolnyk@gmail.com>
2019-05-05 01:03:45 +0300

    first commit in the second repository

---
+++ HW_NEVOLNAR/LM.java
@@ -0,0 +1,19 @@
+package HW_NEVOLNAR;
+
+
+public class LM {
+
+    public static void main (String[] args){
+        int x=2;
+        int y=4;
+        int r=x+y;
+        int z=x+y+r;
+        System.out.println(z);
+
+
+        }
+
+    }
+
+
+
