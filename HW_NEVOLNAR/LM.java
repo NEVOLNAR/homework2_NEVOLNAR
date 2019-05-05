6e4caf5211b4741b9cc92bd113f73d9e31a7a59c
NEVOLNAR <artemnevolnyk@gmail.com>
2019-05-05 22:07:03 +0300

    1st and 2en ex

--- HW_NEVOLNAR/LM.java
+++ HW_NEVOLNAR/LM.java
@@ -1,19 +1,38 @@
 package HW_NEVOLNAR;
 
-
 public class LM {
 
-    public static void main (String[] args){
-        int x=2;
-        int y=4;
-        int r=x+y;
-        int z=x+y+r;
-        System.out.println(z);
+   public static void main (String[] args){
+       //ex1
+       int i=10;
+       int ia=124;
+       System.out.println(lmFac(i,ia));
 
 
-        }
+
+   }
+   //ex1
+       public static int lmFac(int i, int ia){
+
+       int minIandIA=Math.min(i,ia);
+       int maxIandIY=Math.max(i,ia);
+       int iz=0;
+       for (int z=minIandIA;z>iz;z--){
+           if((minIandIA%z==iz)&&(maxIandIY%z==iz)){
+               return z;
+           }
+       }
+       return 1;
+       }
+
+
+
 
     }
+
+
+
+
 
 
 
---
+++ HW_NEVOLNAR/ex2.java
@@ -0,0 +1,22 @@
+package HW_NEVOLNAR;
+
+public class ex2 {
+
+    public static void main(String[]args){
+        //ex2
+        int pi=122;
+        int z=0;
+        int z1=0;
+        int z10=10;
+        System.out.println(z);
+
+            while(pi!=z1){
+                z+=pi%z10;
+                pi/=z10;
+
+            }
+
+        }
+}
+
+
